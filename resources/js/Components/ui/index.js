6f384fa4074549379b793fc6a77599886f116372
Bruno Nunes <brunogilberto.nunes@gmail.com>
2025-11-24 02:09:08 -0300

    Install components and configure shadcn/ui

---
+++ resources/js/Components/ui/index.js
@@ -0,0 +1,66 @@
+// Button
+export { Button, buttonVariants } from './button'
+
+// Input
+export { Input } from './input'
+
+// Textarea
+export { Textarea } from './textarea'
+
+// Card
+export {
+  Card,
+  CardHeader,
+  CardFooter,
+  CardTitle,
+  CardDescription,
+  CardContent,
+} from './card'
+
+// Label
+export { Label } from './label'
+
+// Dialog
+export {
+  Dialog,
+  DialogPortal,
+  DialogOverlay,
+  DialogClose,
+  DialogTrigger,
+  DialogContent,
+  DialogHeader,
+  DialogFooter,
+  DialogTitle,
+  DialogDescription,
+} from './dialog'
+
+// Alert
+export { Alert, AlertTitle, AlertDescription } from './alert'
+
+// Badge
+export { Badge, badgeVariants } from './badge'
+
+// Checkbox
+export { Checkbox } from './checkbox'
+
+// Radio Group
+export { RadioGroup, RadioGroupItem } from './radio-group'
+
+// Select
+export {
+  Select,
+  SelectGroup,
+  SelectValue,
+  SelectTrigger,
+  SelectContent,
+  SelectItem,
+  SelectSeparator,
+  SelectScrollUpButton,
+  SelectScrollDownButton,
+} from './select'
+
+// Tabs
+export { Tabs, TabsList, TabsTrigger, TabsContent } from './tabs'
+
+// Separator
+export { Separator } from './separator'
