1ff43c3b28b8438529995ec27c0f264e68b38c21
Bruno Nunes <brunogilberto.nunes@gmail.com>
2025-11-24 01:16:07 -0300

    Started and configured [Inertia] | [Tailwind] | [Shadcn]

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Inertia\Inertia;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,20 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (!app()->runningInConsole()) {
+            Inertia::share([
+                'user' => fn () => Auth::user() ? [
+                    'id' => Auth::user()->id,
+                    'name' => Auth::user()->name,
+                    'email' =>  Auth::user()->email,
+                ] : null,
+                'appName' => config('app.name'),
+                'flash' => fn () => [
+                    'success' => session('success'),
+                    'error' => session('error'),
+                ],
+                'basePath' => fn () => '/' . request()->segment(1),
+            ]);
+        }
     }
 }
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,8 +1,16 @@
 <?php
 
-use Illuminate\Foundation\Application;
-use Illuminate\Foundation\Configuration\Exceptions;
-use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Foundation\{
+    Application,
+    Configuration\Exceptions,
+    Configuration\Middleware
+};
+
+use App\Http\Middleware\{
+    UserMiddleware,
+    AdminMiddleware,
+    HandleInertiaRequests
+};
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -11,7 +19,13 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->web(append: [
+            HandleInertiaRequests::class,
+        ]);
+        $middleware->alias([
+            'user' => UserMiddleware::class,
+            'admin' => AdminMiddleware::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
