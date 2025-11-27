83df3f6fcb5c612b5eb804e7227635b54ae13d44
Bruno Nunes <brunogilberto.nunes@gmail.com>
2025-11-27 16:07:33 -0300

    Fix dockerfile for node build

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,10 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
+    build: {
+        outDir: 'public/build',
+        emptyOutDir: true,
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.jsx'],
